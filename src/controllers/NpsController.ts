f6a7cd44cccdc4524144453cb94e15e514338a00
Jhonata Augusto <jhon.augustosilva@gmail.com>
2021-02-27 17:46:27 -0300

    first commit

---
+++ src/controllers/NpsController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import { getCustomRepository, Not, IsNull } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import * as yup from "yup";
+import { AppError } from "../errors/AppError";
+
+class NpsController {
+  async execute(request: Request, response: Response) {
+    const { survey_id } = request.params;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      throw new AppError(err)
+    }
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveysUsers = await surveysUsersRepository.find({
+      survey_id,
+      value: Not(IsNull()),
+    });
+
+    const detractor = surveysUsers.filter(
+      (survey) => survey.value >= 0 && survey.value <= 6
+    ).length;
+
+    const promoters = surveysUsers.filter(
+      (survey) => survey.value >= 9 && survey.value <= 10
+    ).length;
+
+    const passive = surveysUsers.filter(
+      (survey) => survey.value >= 7 && survey.value <= 8
+    ).length;
+
+    const totalAnswers = surveysUsers.length;
+
+    const calculate = Number(
+      (((promoters - detractor) / totalAnswers) * 100).toFixed(3)
+    );
+
+    return response.json({
+      detractor,
+      promoters,
+      passive,
+      totalAnswers,
+      nps: calculate,
+    });
+  }
+}
+
+export { NpsController };
